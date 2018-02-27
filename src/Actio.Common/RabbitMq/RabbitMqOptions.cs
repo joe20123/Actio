cbf1caf0f131472c6f2012b14e84fc2c1e5f8cb9
DEXION.AU\joseph.wu <wuzhou88@gmail.com>
2018-02-27 16:06:52 +1100

    added rabitmq service code in startup; created 'activityCreated' event handler

---
+++ src/Actio.Common/RabbitMq/RabbitMqOptions.cs
@@ -0,0 +1,12 @@
+using RawRabbit.Configuration;
+
+namespace Actio.Common.RabbitMq
+{
+    // http://rawrabbit.readthedocs.io/en/master/
+    public class RabbitMqOptions : RawRabbitConfiguration
+    {
+        public RabbitMqOptions()
+        {
+        }
+    }
+}
