f26b68053e986755d8954e4751845e000182fa4b
DEXION.AU\joseph.wu <wuzhou88@gmail.com>
2018-06-01 09:30:47 +1000

    refactor IEncryptor

--- src/Actio.Services.Activities/Domain/Services/IEncryptor.cs
+++ src/Actio.Services.Activities/Domain/Services/IEncryptor.cs
@@ -2,6 +2,7 @@ namespace Actio.Services.Activities.Domain.Services
 {
     public interface IEncryptor
     {
-         
+         string GetSalt(string value);
+         string GetHash(string value, string salt);
     }
 }
