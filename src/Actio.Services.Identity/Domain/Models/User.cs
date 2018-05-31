9f219f3169b487f54f1b2b9d838e412ea2ea397b
Joseph Wu <joe@192-168-1-6.tpgi.com.au>
2018-06-01 08:08:04 +1000

    added new class

--- src/Actio.Services.Identity/Domain/Models/User.cs
+++ src/Actio.Services.Identity/Domain/Models/User.cs
@@ -1,5 +1,22 @@
+using System;
+
 public class User {
 
-    public string Email { get; set; }
-    public string Password { get; set; }
+    protected User()
+    {     
+    }
+    public User(string email, string name )
+    {
+        Id    = Guid.NewGuid();
+        Email = email.ToLowerInvariant();
+        name  = name.ToLowerInvariant();
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public Guid Id { get; protected set; }
+    public string Email { get; protected set; }
+    public string Name { get; protected set; }
+    public string Password { get; protected set; }
+    public string Salt { get; protected set; }
+    public DateTime CreatedAt { get; set; }
 }
